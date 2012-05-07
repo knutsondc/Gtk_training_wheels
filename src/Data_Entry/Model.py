5ec86dbb4951d1764d9f2cb8bcd5986ca8c8bb3e
Darron C. Knutson <darron.knutson@gmail.com>
2012-05-07 13:22:24 -0500

    Floundering around in the Model definition!

--- src/Data_Entry/Model.py
+++ src/Data_Entry/Model.py
@@ -1,15 +1,12 @@
 # Simple data store object definition for use in project intended to help
 # learn how to implement GTK+ 3.0 ListStores and add, delete, and edit theur
-#contents in a Gtk.TreeView
+# contents in a Gtk.TreeView
 
 from gi.repository import Gtk
 
 class RecordsStore(Gtk.ListStore):
     
-    def __init__(self, name, columns):
-        self.columns = columns
-        self.name = name
-        self = Gtk.ListStore(*columns)
+    def __init__(self, columns, names):
         
-        
-        
+        Gtk.ListStore__init__(self, *columns)
+        self.names = names
